5eb4b64b132b805079f3c6d38b80379f2b299787
Moshe Malatji <moshemalatji@gmail.com>
2020-02-01 15:55:44 +0200

    Add project files.

---
+++ QRCodeScanner/QRCodeScanner.Android/Resources/Services/QRScanningService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using QRCodeScanner.QR_Service;
+using ZXing.Mobile;
+using System.Threading.Tasks;
+
+[assembly: Dependency(typeof(QRCodeScanner.Droid.Resources.Services.QRScanningService))]
+
+namespace QRCodeScanner.Droid.Resources.Services
+{
+    public class QRScanningService : IQrScanningService
+    {
+        public async Task<string> ScanAsync()
+        {
+            var optionsDefault = new MobileBarcodeScanningOptions();
+            var optionsCustom = new MobileBarcodeScanningOptions();
+
+            var scanner = new MobileBarcodeScanner()
+            {
+            TopText = "Scan the QR Code",
+            BottomText = "Please Wait"
+            };
+
+            var scanResult = await scanner.Scan(optionsCustom);
+            return scanResult.Text;
+        }
+    }
+}
---
+++ QRCodeScanner/QRCodeScanner/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using QRCodeScanner.QR_Service;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace QRCodeScanner
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnScan_Clicked(object sender,EventArgs e)
+        {
+            try
+            {
+                var scanner = DependencyService.Get<IQrScanningService>();
+                var result = await scanner.ScanAsync();
+                if (result != null)
+                {
+                    txtBarcode.Text = result;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            
+        }
+    }
+}
